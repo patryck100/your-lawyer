b4a9c1127099defed1621221d1e73bf94620e465
Patryck Brenner <patryck_brenner@outlook.com>
2021-08-12 01:23:25 +0100

    Adapting button on sign in component style for small screens

--- src/components/sign-in/sign-in.styles.jsx
+++ src/components/sign-in/sign-in.styles.jsx
@@ -13,4 +13,10 @@ export const SignInTitle = styled.h2`
 export const ButtonsBarContainer = styled.div`
   display: flex;
   justify-content: space-between;
+  
+  @media screen and (max-width: 443px) {
+    display: block;
+    paddin: 0 10px;
+    min-width: unset;
+  }
 `;
