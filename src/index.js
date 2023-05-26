72a1b47ef8bf870850de90ebbe3a663eb12e4ec0
Patryck Brenner <patryck_brenner@outlook.com>
2023-05-26 13:13:38 +0100

    Adding redirect for Netlify

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
-
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
+/* ReactDOM.render(
   //BrowserRouter is a component that gives the functionalities of the react router to the app
   //because the provider is surrending everything and passing "store" as props, it allows anything inside to access the redux
   <Provider store={store}>
@@ -17,4 +17,14 @@ ReactDOM.render(
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
+); */
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
